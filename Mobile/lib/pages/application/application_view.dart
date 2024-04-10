6add9cd6d204099a0d83cae004b70c48c26f6bc6
duckhai2323 <tranduckhai26112003@gmail.com>
2024-04-11 01:48:30 +0700

    Create Message Page Candidate

--- Mobile/lib/pages/application/application_view.dart
+++ Mobile/lib/pages/application/application_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:jobpilot_app/pages/application/message/message_view.dart';
 
 import '../../common/colors/colors.dart';
 import 'application_controller.dart';
@@ -22,7 +23,7 @@ class ApplicationPage extends GetView<ApplicationController>{
         children: [
           HomePage(),
           Center(child: Text('CV'),),
-          Center(child: Text('CHAT'),),
+          MessagePage(),
           Center(child: Text('INTERVIEW'),),
           Center(child: Text('PROFILE'),),
         ],
---
+++ Mobile/lib/pages/application/message/list_contact.dart
@@ -0,0 +1,104 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:jobpilot_app/common/colors/colors.dart';
+
+import 'message_controller.dart';
+
+class ListContact extends GetView<MessageController>{
+  Widget ItemUser1 (){
+    return Column(
+      children: [
+        ListTile(
+          leading: Stack(
+            children: [
+              CircleAvatar(
+                radius: 39,
+                backgroundColor: Colors.lightBlueAccent,
+                child:Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage('https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/429676880_249901944842691_293763202580111610_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH60vAWV-kWtMcuQCB2lUQ7ikwEH4IHee-KTAQfggd579lagOCQfl2kw6QtWE7C6UVgf0Ua-7zr9VnRZzNQnqMY&_nc_ohc=TLLAW6gjvjoAb6JXV0U&_nc_ht=scontent.fhan14-1.fna&cb_e2o_trans=t&oh=00_AfCwgrLbMynSGBmuS-J-h3whUt81EDrt8Tq1nX6ppersQw&oe=661C9C94'),
+                      ),
+                    ),
+                  ),
+                ) ,
+              ),
+              const Positioned(
+                right: 5,
+                bottom: 3,
+                child: CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 6,
+                    backgroundColor: Color(0xFF4CAF50),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          title: const Text(
+            "Nguyen Van B",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+
+          subtitle: const Text(
+            'May da chay task xong chua ha',
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight:  FontWeight.bold,
+            ),
+          ),
+
+          trailing: const CircleAvatar(
+            radius: 7,
+            backgroundColor: AppColors.primaryColor1,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Divider(
+          thickness: 2,
+          indent: 20,
+          endIndent: 20,
+          color: Colors.grey.withOpacity(0.5),
+        ),
+      ],
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10,top: 5,bottom: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        itemCount: 5,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (BuildContext context, int index){
+          return InkWell(
+              onTap:(){
+                //controller.ClickItemChat(controller.listUser[index]);
+              } ,
+              child: ItemUser1());
+        },
+      ),
+    );
+  }
+}
