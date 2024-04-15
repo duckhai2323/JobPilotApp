a190b6118bd27b9802c35565b24e2521edb224c6
duckhai2323 <tranduckhai26112003@gmail.com>
2024-04-16 00:07:16 +0700

    Complete Api SignIn SignUp Simple

---
+++ Mobile/lib/common/api/api_backend.dart
@@ -0,0 +1,6 @@
+import 'package:jobpilot_app/common/api/auth_account.dart';
+
+class ApiEndPoints {
+  static final String baseUrl = 'http://10.0.2.2:8000/api/';
+  static AuthAccount authAccount = AuthAccount();
+}
---
+++ Mobile/lib/common/api/auth_account.dart
@@ -0,0 +1,4 @@
+class AuthAccount {
+  final String REGISTER_CANDIDATE = 'candidate/register';
+  final String SIGNIN_CANDIDATE = 'candidate/signin';
+}
