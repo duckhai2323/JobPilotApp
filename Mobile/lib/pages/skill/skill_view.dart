a823603f73d6fc378019e9513f7bf9381ad51d86
Noblesse1607 <luongduc0912511093@gmail.com>
2024-05-14 11:26:18 +0700

    UpdateUICertificate And UIInfo

--- Mobile/lib/pages/skill/skill_view.dart
+++ Mobile/lib/pages/skill/skill_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:jobpilot_app/common/colors/colors.dart';
 import 'package:jobpilot_app/pages/skill/skill_controller.dart';
 
 class SkillPage extends GetView<SkillController> {
@@ -7,6 +8,43 @@ class SkillPage extends GetView<SkillController> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        bottomSheet: Container(
+        height: 70,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.8),
+            spreadRadius: 0.25,
+            blurRadius: 3,
+            offset: Offset(0, 2),
+          )
+        ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width - 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: AppColors.primaryColor1,
+                ),
+                child: Center(
+                  child: Text(
+                    'Thêm mới',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
         appBar: AppBar(
         centerTitle: true, // Đặt centerTitle thành true để căn giữa title
           title: Text('Thêm kỹ năng',
@@ -81,25 +119,7 @@ class SkillPage extends GetView<SkillController> {
                     border: InputBorder.none,
                   ),
                 ),
-              ),
-              SizedBox(height: 50),
-              Expanded( // Sử dụng Expanded để nút "Thêm mới" nằm ở dưới cùng và có chiều ngang gần bằng chiều ngang của giao diện
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Xử lý sự kiện khi nút được nhấn
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder( // Thiết lập hình dạng của nút thành chữ nhật
-                      borderRadius: BorderRadius.circular(8),
-                     ),
-                    ),
-                    child: Text('Thêm mới'),
-                  ),
-                ),
-              ),
+              ),  
             ],
           ),
         ),
